58d63a29bf8f971ee638ba930514905efd1bd89b
曹珂俭 <1849201815@qq.com>
2023-02-06 15:09:54 +0800

    补充数据

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,33 +4,45 @@
     <div class="menu">
       <el-menu active-text-color="#ffd04b" background-color="#545c64" class="el-menu-vertical-demo" default-active="2"
         text-color="#fff" @open="handleOpen" @close="handleClose">
-        <el-sub-menu index="1">
+        <el-sub-menu index="1" v-for="menus in  newMenus" :key="menus.id">
           <template #title>
-            <span>Navigator One</span>
+            <span>{{ menus.title }}</span>
           </template>
-          <el-menu-item index="1-4-1">1</el-menu-item>
-          <el-menu-item index="1-4-1">1</el-menu-item>
-          <el-menu-item index="1-4-1">1</el-menu-item>
-          <el-menu-item index="1-4-1">1</el-menu-item>
+          <el-menu-item index="1-4-1" v-for="submenu in menus.children" :key="submenu.id">{{ submenu.title }}</el-menu-item>
         </el-sub-menu>
 
-        
+
       </el-menu>
     </div>
     <div class="content">右侧内容</div>
   </div>
 </template>
 <script setup lang='ts'>
-import { ref } from 'vue'
+import { ref ,onMounted} from 'vue'
 import 'animate.css';
+import { storeToRefs } from 'pinia'
+import { useMainStore } from "../../stores";
+const mainStore = useMainStore();
 
+interface State {
+  menus:MenuObj[]
+}
+interface MenuObj {
+  parentId:number
+  id:number
+  title?:string
+  children?:MenuObj[]
+}
+type NewMenus = {
+  [key:number]:MenuObj
+}
+const newMenus:NewMenus = mainStore.getNewMenus
 const handleOpen = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
 const handleClose = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
-
 </script>
 <style lang='less' scoped>
 .container {
